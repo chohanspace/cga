53dd8cdb6edb2beac47b617e1d25d5dfbabf94bb
Abdullah Chohan <business.abdullahchohan@gmail.com>
2025-06-17 02:49:52 +0000

    the app is not fitting in  mobile so make it to fit in the mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="font-sans antialiased">
         <AuthProvider>
